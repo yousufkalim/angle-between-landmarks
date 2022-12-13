007ff59ff5bbf9bd6866d2add480572fdbd664b3
Yousuf Kalim <yousufkalim@outlook.com>
2022-12-13 15:33:08 +0500

    Tests added

---
+++ tests/index.test.js
@@ -0,0 +1,14 @@
+import { expect, test } from '@jest/globals';
+import findAngle from '../src/index.js';
+
+const landmarks1 = { x: 0, y: 0 };
+const landmarks2 = { x: 1, y: 0 };
+const landmarks3 = { x: 1, y: 1 };
+
+test('Should test angle function with small', () => {
+  expect(findAngle(landmarks1, landmarks2, landmarks3)).toBe(270);
+});
+
+test('Should test angle function with small', () => {
+  expect(findAngle(landmarks1, landmarks2, landmarks3, { small: true, round: true })).toBe(90);
+});
